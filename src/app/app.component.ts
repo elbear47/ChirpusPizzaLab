9031fff1cad1217d8765807d9fe6b9aea1d24dae
Elber Funez <elberfunez@gmail.com>
2022-10-31 20:11:40 -0400

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './MenuItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'menu';
+  stringSearch: string = "";
+
+  menuItems: MenuItem[] = [
+    {
+      item: 'Chicken Fingers',
+      category: 'dinner',
+      price: 11.99,
+    },
+    {
+      item: 'Pizza',
+      category: 'dinner',
+      price: 11.99,
+    },
+    {
+      item: 'Wings',
+      category: 'sides',
+      price: 8.99,
+    },
+    {
+      item: 'Breadsticks',
+      category: 'sides',
+      price: 4.99,
+    },
+    {
+      item: 'Ceaser salad',
+      category: 'salads',
+      price: 5.99,
+    },
+    {
+      item: 'Cinnamon Roll',
+      category: 'dessert',
+      price: 8.99,
+    },
+  ];
+
+
+
+
 }
